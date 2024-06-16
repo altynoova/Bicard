a0a36c7885d592ad2d24bf9d34befbe4d361fdb3
altynoova <you@example.com>
2024-06-16 23:24:18 +0600

    1

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,6 @@
 export const BASE_URL =
   process.env.NODE_ENV === "production"
-    ? "https://disin-react.envytheme.com"
+    ? "https://localhost:7120/api"
     : "https://localhost:7120/api";
 
     export  const url = "https://localhost:7120"
